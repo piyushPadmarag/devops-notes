f487689b350381c07896b0eb7f92ca5730093268
Piyush Padmarag <piyush.padmarag@globallogic.com>
2024-05-13 19:47:56 +0530

    ne

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,1 @@
-const circle = {
-    radius: 1,
-    draw () {
-        console.log('draw');
-    }
-};
-
-for (let key in circle)
-console.log(key, circle[key]);
-
-for (let key of Object.keys(circle))
-console.log(key);
-
-for (let entry of Object.entries(circle))
-console.log(entry);
-
-if ('radius' in circle) console.log('yes');
+const date = new Date('11 may 2024');
