fa144e3efc5c3241be113d9adea890cd6bd27f04
Piyush Padmarag <piyush.padmarag@globallogic.com>
2024-05-13 20:51:19 +0530

    .

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-const date = new Date('11 may 2024');
+let numbers = [1, 2, 3, 4];
+let another = numbers;
+//solution 1:
+
+//numbers = [];
+
+//console.log(numbers);
+
+//console.log(another);
+
+//solution 2:
+
+//numbers.length = 0;
+
+// solution 3:
+
+//numbers.splice(0, numbers.length);
+
+//solution 4:
+
+while(numbers.length >0)
+numbers.pop ();
